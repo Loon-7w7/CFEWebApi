e4ab59e5c18a33b95e453bb55e831839c8aa3f0e
Luis Ku <dual.luis.ku@gmail.com>
2023-05-24 15:00:42 -0600

    creacion de los bibliotecas de clase y entidad de materiales

---
+++ CFE-Domain/Enum/UnitEmun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFE_Domain.Enum
+{
+    public enum UnitEmun
+    {
+        PZA = 0,
+        M = 1,
+        KG = 2,
+        JGO = 3,
+        L = 4,
+    }
+}
---
+++ CFE-Domain/Material/Material.cs
@@ -0,0 +1,36 @@
+﻿using CFE_Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFE_Domain.Material
+{
+    /// <summary>
+    /// Entidad de materiales
+    /// </summary>
+    public class Material
+    {
+        /// <summary>
+        /// Identificador del material
+        /// </summary>
+        public Guid Id { get; set; }
+        /// <summary>
+        /// Folio del material
+        /// </summary>
+        public long Folio {get; set; }
+        /// <summary>
+        /// Nombre del material
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Lote del material
+        /// </summary>
+        public string Lot { get; set; }
+        /// <summary>
+        /// Unidad de medida del material
+        /// </summary>
+        public UnitEmun Unit { get; set; }
+    }
+}
