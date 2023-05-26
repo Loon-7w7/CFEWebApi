7c25eaca7d8aa7489d673681005a19540404bf15
Luis Ku <dual.luis.ku@gmail.com>
2023-05-25 23:22:06 -0600

    servicios de materiales

---
+++ CFE-Services/Implementacion/MaterialService.cs
@@ -0,0 +1,73 @@
+﻿using CFE_Domain.Material;
+using CFE_Services.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFE_Services.Implementacion
+{
+    /// <summary>
+    /// Servio de materiales
+    /// </summary>
+    public class MaterialService : IMaterialRepository
+    {
+        /// <summary>
+        /// Repositorio de materiales
+        /// </summary>
+        private readonly IRepository<Material> _materialRepository;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="materialRepository"></param>
+        public MaterialService(IRepository<Material> materialRepository)
+        {
+            _materialRepository = materialRepository;
+        }
+        /// <summary>
+        /// Crea un material
+        /// </summary>
+        /// <param name="material">Datos del material</param>
+        /// <returns></returns>
+        public Task Add(Material material)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Elimina un material
+        /// </summary>
+        /// <param name="id">id del material</param>
+        /// <returns></returns>
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// obtiene una lista de materiales
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<Material>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Busxas por id del material
+        /// </summary>
+        /// <param name="id">id del material</param>
+        /// <returns></returns>
+        public Task<Material> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Actuliza el material
+        /// </summary>
+        /// <param name="material">Material Actualizado</param>
+        /// <returns></returns>
+        public Task Update(Material material)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CFE-Services/Repositorios/IMaterialRepository.cs
@@ -0,0 +1,46 @@
+﻿using CFE_Domain.Material;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CFE_Services.Repositorios
+{
+    /// <summary>
+    /// Repositorio de materiales
+    /// </summary>
+    public interface IMaterialRepository
+    {
+        /// <summary>
+        /// Busxas por id del material
+        /// </summary>
+        /// <param name="id">id del material</param>
+        /// <returns></returns>
+        Task<Material> GetById(Guid id);
+        /// <summary>
+        /// obtiene una lista de materiales
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Material>> GetAll();
+        /// <summary>
+        /// Crea un material
+        /// </summary>
+        /// <param name="material">Datos del material</param>
+        /// <returns></returns>
+        Task Add(Material material);
+        /// <summary>
+        /// Actuliza el material
+        /// </summary>
+        /// <param name="material">Material Actualizado</param>
+        /// <returns></returns>
+        Task Update(Material material);
+        /// <summary>
+        /// Elimina un material
+        /// </summary>
+        /// <param name="id">id del material</param>
+        /// <returns></returns>
+        Task Delete(Guid id);
+    }
+}
